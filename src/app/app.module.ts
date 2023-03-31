5223fbbfcff76c46a8de787e43b38b326865183d
John Manoah <john@vanarts.com>
2023-03-31 11:58:19 -0700

    Wk3 homework Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { MainSectionComponent } from './main-section/main-section.component';
+import { SideSectionComponent } from './side-section/side-section.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    MainSectionComponent,
+    SideSectionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
